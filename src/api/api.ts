868ae98c992b1bcab24015acf174b9bb0f28193d
satera94da025 <bogdanhorovcov0@gmail.com>
2021-04-14 21:07:27 +0300

    update

---
+++ src/api/api.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+import {UserType} from '../types/types';
+
+export const instance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers:     {
+        "API-KEY": "63e080d1-f004-48f7-ae2d-df9d85d2ae65"
+    }
+});
+
+export enum ResultCodesEnum {
+    Success = 0,
+    Error = 1
+}
+
+export enum ResultCodeForCapcthaEnum {
+    CaptchaIsRequired = 10
+}
+
+export type GetItemsType = {
+    items: Array<UserType>
+    totalCount: number
+    error: string | null
+}
+export type APIResponseType<D = {}, RC = ResultCodesEnum> = {
+    data: D
+    messages: Array<string>
+    resultCode: RC
+}
